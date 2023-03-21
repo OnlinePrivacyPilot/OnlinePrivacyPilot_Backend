95b9a36f685d28f5388426dace8692465f0424ce
mablr <mablr@pm.me>
2023-03-21 10:46:00 +0100

    Modify storage to handle all input target types

--- src/storage.py
+++ src/storage.py
@@ -2,21 +2,42 @@ from simple_graph_sqlite import database as db
 from simple_graph_sqlite.visualizers import graphviz_visualize
 from typing import Optional
 from sqlite3 import IntegrityError
+import unicodedata
+import string
 
 class Storage:
-    def __new__(cls, db_file: Optional[str] = None):
+    def __new__(cls, target: Optional[str] = None):
         if not hasattr(cls, 'instance'):
             cls.instance = super(Storage, cls).__new__(cls)
             cls.footprint_id = 0
-            cls.db_file = db_file
+            cls.db_file = cls.get_filename(target)
             cls.db_conn = db.initialize(cls.db_file)
         return cls.instance
 
+    def get_filename(target: str) -> str:
+        """
+        Uses target value to generate a name for the DB file
+
+        Args:
+            target (str): Input target
+
+        Returns:
+            str: Name for DB file
+        """
+        filename = unicodedata.normalize('NFKD', target).encode('ASCII', 'ignore').decode()
+        return ''.join(char for char in filename if char in "-_ ().%s%s" % (string.digits, string.ascii_letters))
+        
     def store_node(self, method: str, type: str, value: str) -> int:
         """
-        In that function we store a node in the graph.
+        In that function we store a footprint in the graph.
 
-        :return: int
+        Args:
+            method (str): Method used to retieve footprint
+            type (str): Type of footprint
+            value (str): Value of footprint
+
+        Returns:
+            int: ID corresponding to stored node
         """
         self.footprint_id += 1
         try:
@@ -26,11 +47,13 @@ class Storage:
             db.atomic(self.db_file, db.add_node({'method': method, 'type': type, 'value': value}, self.footprint_id))
         return self.footprint_id
 
-    def store_edge(self, parent_id, child_id) -> None:
+    def store_edge(self, parent_id: int, child_id: int) -> None:
         """
         In that function we store an edge in the graph.
 
-        :return: None
+        Args:
+            parent_id (int): Parent node ID
+            child_id (int): Child node ID
         """
         db.atomic(self.db_file, db.connect_nodes(parent_id, child_id, {}))
 
