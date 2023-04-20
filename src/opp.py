532e1b018f91878e4c5dd7101da4b591ace05790
mablr <mablr@pm.me>
2023-04-20 11:19:37 +0200

    Minor fixes on OPP module

--- src/opp.py
+++ src/opp.py
@@ -3,12 +3,12 @@ from src import storage
 from typing import Optional
 
 class OPP:
-    def __init__(self, target: str = None, target_type: Optional[str] = None, search_depth: int = 3, initial_filter: list = None):
+    def __init__(self, target: str = None, target_type: Optional[str] = None, search_depth: Optional[int] = 3, initial_filters: list = None):
         self.target = target
         self.target_type = target_type
         self.search_depth = search_depth
         self.storage = storage.Storage(target.replace(' ', '_')+".db")
-        self.fingerprint = footprint.RecursionHandler.get_root(target=self.target, search_depth=search_depth, initial_filters=initial_filter)
+        self.fingerprint = footprint.RecursionHandler.get_root(target=self.target, search_depth=search_depth, initial_filters=initial_filters)
 
 
     def get_fingerprint(self):
