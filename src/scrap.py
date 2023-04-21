d2d98cdb84a5324e750a95e3e394b57284374994
b3noix <benoit.leconte@grenoble-inp.org>
2023-04-21 13:19:32 +0200

    removing scrapping of the image on LinkedIn

--- src/scrap.py
+++ src/scrap.py
@@ -367,17 +367,6 @@ class LinkedinScrapper(AbstractScrapper):
         except NoSuchElementException:
             pass
 
-        try:
-            result.append(
-                {
-                    "type" : "image",
-                    "value" : driver.find_element(By.CLASS_NAME, "artdeco-entity-image").get_attribute("src"),
-                    "method" : self.METHOD_NAME
-                }
-            )
-        except NoSuchElementException:
-            pass
-
         driver.quit()
         return result
     
