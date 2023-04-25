842042e79b08524ae26a403649bd77db528a03af
mablr <mablr@pm.me>
2023-04-25 14:42:57 +0200

    back to full list SEARCHABLE_TYPES

--- src/ftype.py
+++ src/ftype.py
@@ -1,6 +1,5 @@
 MAIN_FILTERS = ["name", "username"]
 FILTERS = ["email", "location", "phone", "occupation"]
 SCRAPABLE_TYPES = ["url"]
-#SEARCHABLE_TYPES = FILTERS + MAIN_FILTERS
-SEARCHABLE_TYPES = ["name"]
+SEARCHABLE_TYPES = FILTERS + MAIN_FILTERS
 SCRAP_RETURN = ["name", "username","description", "location", "url", "birthdate", "image", "occupation", "company"]
